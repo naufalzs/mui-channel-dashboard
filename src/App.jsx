5cbf47e46830f3741ae6760aaa6ca0e3ec6ff737
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-10-18 09:11:11 +0700

    feat: setup material ui theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { CssBaseline, Typography } from "@mui/material";
+import { CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import theme from "./config/theme";
 
 function App() {
   return (
-    <CssBaseline>
-      <Typography>App</Typography>
-    </CssBaseline>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Typography>App</Typography>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
