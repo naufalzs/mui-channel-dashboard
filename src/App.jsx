f77bee993eff6b94c34c707b931ef23c43876b1d
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-10-18 10:21:13 +0700

    feat: set app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import theme from "./config/theme";
+import Sidenav from "./components/Sidenav";
+import AppHeader from "./components/AppHeader";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline>
-        <Typography>App</Typography>
+        <AppHeader />
+        <Box sx={styles.container}>
+          <Sidenav />
+          <Box component={"main"}></Box>
+        </Box>
       </CssBaseline>
     </ThemeProvider>
   );
 }
+
+/** @type {import("@mui/material").SxProps} */
+const styles = {
+  container: {
+    display: "flex",
+    bgcolor: "neutral.light",
+    height: "calc(100vh - 64px)",
+  },
+};
 
 export default App;
